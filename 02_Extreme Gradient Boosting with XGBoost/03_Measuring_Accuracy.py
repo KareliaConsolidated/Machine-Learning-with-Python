6b10e4982036edf42f3917394870a3bbb886d24a
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-29 10:14:46 +0530

    Measuring Accuracy

---
+++ 02_Extreme Gradient Boosting with XGBoost/03_Measuring_Accuracy.py
@@ -0,0 +1,34 @@
+# Measuring accuracy
+# You'll now practice using XGBoost's learning API through its baked in cross-validation capabilities. As Sergey discussed in the previous video, XGBoost gets its lauded performance and efficiency gains by utilizing its own optimized data structure for datasets called a DMatrix.
+# In the previous exercise, the input datasets were converted into DMatrix data on the fly, but when you use the xgboost cv object, you have to first explicitly convert your data into a DMatrix. So, that's what you will do here before running cross-validation on churn_data.
+# Create arrays for the features and the target: X, y
+X, y = churn_data.iloc[:,:-1], churn_data.iloc[:,-1]
+
+# Create the DMatrix from X and y: churn_dmatrix
+churn_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:logistic", "max_depth":3}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=churn_dmatrix, params=params, 
+                  nfold=3, num_boost_round=5, 
+                  metrics="error", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Print the accuracy
+print(((1-cv_results["test-error-mean"]).iloc[-1]))
+# cv_results stores the training and test mean and standard deviation of the error per boosting round (tree built) as a DataFrame. From cv_results, the final round 'test-error-mean' is extracted and converted into an accuracy, where accuracy is 1-error. The final accuracy of around 75% is an improvement from earlier!
+# Perform cross_validation: cv_results
+cv_results = xgb.cv(dtrain=churn_dmatrix, params=params, 
+                  nfold=3, num_boost_round=5, 
+                  metrics="auc", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Print the AUC
+print((cv_results["test-auc-mean"]).iloc[-1])
+# An AUC of 0.84 is quite strong. As you have seen, XGBoost's learning API makes it very easy to compute any metric you may be interested in. In Chapter 3, you'll learn about techniques to fine-tune your XGBoost models to improve their performance even further. For now, it's time to learn a little about exactly when to use XGBoost.
