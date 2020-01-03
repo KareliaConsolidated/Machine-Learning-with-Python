fd70388a7a70c8ad368d85257c8c06ee333216c8
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-01-03 19:36:23 +0530

    Unsupervised Learning

--- 03_Clustering Methods with SciPy/01_Unsupervised_Learning.py
+++ 03_Clustering Methods with SciPy/01_Unsupervised_Learning.py
@@ -43,3 +43,58 @@ df['cluster_labels'], _ = vq(df, centroids)
 sns.scatterplot(x='x', y='y', hue='cluster_labels', data=df)
 plt.show()
 
+# Normalize basic list data
+# Now that you are aware of normalization, let us try to normalize some data. goals_for is a list of goals scored by a football team in their last ten matches. Let us standardize the data using the whiten() function.
+# Import the whiten function
+from scipy.cluster.vq import whiten
+
+goals_for = [4,3,2,3,1,1,2,0,1,4]
+
+# Use the whiten() function to standardize the data
+scaled_data = whiten(goals_for)
+print(scaled_data)
+
+# Visualize normalized data
+# After normalizing your data, you can compare the scaled data to the original data to see the difference. The variables from the last exercise, goals_for and scaled_data are already available to you.
+# Plot original data
+plt.plot(goals_for, label='original')
+
+# Plot scaled data
+plt.plot(scaled_data, label='scaled')
+
+# Show the legend in the plot
+plt.legend()
+
+# Display the plot
+plt.show()
+
+# Normalization of small numbers
+# In earlier examples, you have normalization of whole numbers. In this exercise, you will look at the treatment of fractional numbers - the change of interest rates in the country of Bangalla over the years. For your use, matplotlib.pyplot is imported as plt.
+# Prepare data
+rate_cuts = [0.0025, 0.001, -0.0005, -0.001, -0.0005, 0.0025, -0.001, -0.0015, -0.001, 0.0005]
+
+# Use the whiten() function to standardize the data
+scaled_data = whiten(rate_cuts)
+
+# Plot original data
+plt.plot(rate_cuts, label='original')
+
+# Plot scaled data
+plt.plot(scaled_data, label='scaled')
+
+plt.legend()
+plt.show()
+
+# FIFA 18: Normalize data
+# FIFA 18 is a football video game that was released in 2017 for PC and consoles. The dataset that you are about to work on contains data on the 1000 top individual players in the game. You will explore various features of the data as we move ahead in the course. In this exercise, you will work with two columns, eur_wage, the wage of a player in Euros and eur_value, their current transfer market value.
+# The data for this exercise is stored in a Pandas dataframe, fifa. whiten from scipy.cluster.vq and matplotlib.pyplot as plt have been pre-loaded.
+# Scale wage and value
+fifa['scaled_wage'] = whiten(fifa['eur_wage'])
+fifa['scaled_value'] = whiten(fifa['eur_value'])
+
+# Plot the two columns in a scatter plot
+fifa.plot(x='scaled_wage', y='scaled_value', kind = 'scatter')
+plt.show()
+
+# Check mean and standard deviation of scaled values
+print(fifa[['scaled_wage', 'scaled_value']].describe())
