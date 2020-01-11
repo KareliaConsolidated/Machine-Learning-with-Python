11e4da9096b553ebaf80dc4739d3a158bb1988f1
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-01-11 20:32:32 +0530

    Regularization

--- 04_Dimensionality Reduction in Python/05_Regularization.py
+++ 04_Dimensionality Reduction in Python/05_Regularization.py
@@ -37,3 +37,18 @@ n_ignored = sum(zero_coef)
 print("The model has ignored {} out of {} features.".format(n_ignored, len(la.coef_)))
 
 # We can predict almost 85% of the variance in the BMI value using just 9 out of 91 of the features. The R^2 could be higher though.
+
+# Adjusting the regularization strength
+# Your current Lasso model has an R2 score of 84.7%. When a model applies overly powerful regularization it can suffer from high bias, hurting its predictive power.
+# Let's improve the balance between predictive power and model simplicity by tweaking the alpha parameter.
+# Find the highest alpha value with R-squared above 98%
+la = Lasso(alpha=0.1, random_state=0)
+
+# Fits the model and calculates performance stats
+la.fit(X_train_std, y_train)
+r_squared = la.score(X_test_std, y_test)
+n_ignored_features = sum(la.coef_ == 0)
+
+# Print peformance stats 
+print("The model can predict {0:.1%} of the variance in the test set.".format(r_squared))
+print("{} out of {} features were ignored.".format(n_ignored_features, len(la.coef_)))
